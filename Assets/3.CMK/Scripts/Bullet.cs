868228ea6c2e866b85a823598a47da33af30e6eb
choimingi <choimingi0412@gmail.com>
2026-04-09 21:12:57 +0900

    feat: 서바이벌 게임
    1. BulletSpawner
    - 랜덤한 값을 주어 여러 개 생성
    - 코루틴을 이용하여 생성에 텀을 둠
    - Physics.OverlapSphere를 이용하여 생성될 때 안겹치게 생성
    
    2. Bullet
    - Physics.IgnoreLayerCollision를 통해 특정 Layer끼리 충돌을 안하게 설정

--- Assets/3.CMK/Scripts/Bullet.cs
+++ Assets/3.CMK/Scripts/Bullet.cs
@@ -2,17 +2,21 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public float speed = 10f;
+    public float speed = 4.5f;
     private Rigidbody rb;
 
     void Start()
     {
+
+        Physics.IgnoreLayerCollision( LayerMask.NameToLayer("Bullet"),LayerMask.NameToLayer("Bullet"),true); // Bullet끼리는 통과되게 함
+
         rb = GetComponent<Rigidbody>();
 
         rb.linearVelocity = transform.forward * speed;
 
         Destroy(gameObject, 2.5f);
     }
+
 
     public void Reflect(Vector3 dir)
     {
--- Assets/3.CMK/Scripts/SpawnerManager.cs
+++ Assets/3.CMK/Scripts/SpawnerManager.cs
@@ -1,11 +1,14 @@
+using System.Collections;
 using UnityEngine;
 
 public class SpawnerManager : MonoBehaviour
 {
     public GameObject spawnerPrefab;
-    public float spawnInterval = 3f;
+    private float spawnInterval;
 
-    public float radius = 6f; //  반지름
+    public float radius = 6.0f; //  반지름
+    public float minDistance = 2.0f; // 생성되었을 때 2.0f 거리만큼에 오브젝트가 있는지
+    private int SpawnCount;     // BulletSpawner 개수
 
     private float timer = 0f;
 
@@ -13,24 +16,46 @@ public class SpawnerManager : MonoBehaviour
     {
         timer += Time.deltaTime;
 
-        if (timer >= spawnInterval)
+        spawnInterval = Random.Range(2.5f, 4.5f);  // 2.5 ~ 4.5초 사이에 랜덤한 값 지정
+        SpawnCount = Random.Range(1, 5);           // 1 ~ 5개 중 랜덤한 값 지정
+
+        if (timer >= spawnInterval) // timer가 랜덤한 값보다 크거나 같으면 코루틴 시작
         {
             timer = 0f;
-            SpawnSpawner();
+
+            StartCoroutine(SpawnDelay());
+            
         }
     }
 
-    void SpawnSpawner()
+    IEnumerator SpawnDelay() // spawner 생성 
     {
-        //  랜덤 방향 (XZ 평면)
-        Vector2 randomDir = Random.insideUnitCircle.normalized;
+        for (int i = 0; i < SpawnCount; i++) // 랜덤한 spawnCount만큼 Spawner 생성
+        {
+            SpawnSpawner();
 
-        //  반지름 6 위치
-        Vector3 spawnPos = new Vector3(randomDir.x, 0, randomDir.y) * radius;
+            yield return new WaitForSeconds(0.3f); // 생성할 때 텀을 주어 생성
+        }
+    }
+    void SpawnSpawner() // spawner
+    {
+        int maxTry = 10; // 무한루프 방지
 
-        //  중심 기준으로 이동
-        spawnPos += transform.position;
+        for (int i = 0; i < maxTry; i++)
+        {
+            Vector2 randomDir = Random.insideUnitCircle.normalized;               // 원 안에 랜덤 방향 정하기
+            Vector3 spawnPos = new Vector3(randomDir.x, 0, randomDir.y) * radius; // 방향 * 반지름 원의 둘레 위치
+            spawnPos += transform.position;   // Spawner를 기준으로 이동
 
-        Instantiate(spawnerPrefab, spawnPos, Quaternion.identity);
+            int layerMask = LayerMask.GetMask("Spawner");
+
+            Collider[] cols = Physics.OverlapSphere(spawnPos, minDistance, layerMask); // Spawner layer를 가진 오브젝트들을 찾기
+
+            if (cols.Length == 0) // 오브젝트를 찾고 겹치는 오브젝트가 없다면 생성
+            {
+                Instantiate(spawnerPrefab, spawnPos, Quaternion.identity);
+                return;
+            }
+        }
     }
 }
