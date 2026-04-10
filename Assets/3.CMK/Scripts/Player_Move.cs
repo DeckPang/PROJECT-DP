e960c1806b50b236ff1de7a441bfb6b063ba7599
choimingi <choimingi0412@gmail.com>
2026-04-10 18:29:46 +0900

    feat: 서바이벌 게임
    1. Player
    - 원점을 기준으로 반지름 10만큼까지만 이동제한
    - Bullet에 맞으면 날아가도록 설정 (Destroy대신 게임 오버되는걸 표현)

--- Assets/3.CMK/Scripts/Player_Move.cs
+++ Assets/3.CMK/Scripts/Player_Move.cs
@@ -4,28 +4,58 @@ public class Player_Move : MonoBehaviour
 {
     [Header("Move")]
     public float speed = 2.0f;
+    public int moveRange = 10;
 
     [Header("Look Rotate")]
     public float rotateSpeed = 10f;
     public float stopDistance = 0.5f;
 
+    private bool gameOver;
     private Rigidbody rb;
 
     void Start()
     {
+        gameOver = false;
         rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        Move();
-        Look();
+        if (gameOver != true)
+        {
+            Move();
+            Look();
+        }
+        
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Bullet"))
+        {
+            gameOver = true;
+            Vector3 dir = transform.position - collision.transform.position;
+            dir = dir.normalized;
+
+            dir.y = 0.5f;
+            float force = 30f;
+            rb.AddForce(dir * force, ForceMode.Impulse);
+        }
     }
 
     void Move()
     {
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
+
+        Vector3 pos = transform.position; // 현재 위치
+
+        if (pos.magnitude > moveRange) // 원점으로부터 moveRange를 넘었을 때
+        {
+            transform.position = pos.normalized * moveRange;
+        }
+
 
         // 월드 좌표 기준 이동
         Vector3 move = new Vector3(h, 0, v);
